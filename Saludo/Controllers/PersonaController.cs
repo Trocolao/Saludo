1f0b492fe5e18bc8f13c661bd86757db91b0fe03
Trocolao <daw13.2023@gmail.com>
2023-03-28 11:06:38 +0200

    Carlos Muñoz proyecto mvc de saludo de una persona en csharp

---
+++ Saludo/Controllers/PersonaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Saludo.Models;
+
+namespace Saludo.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Persona()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Saludo(Persona persona)
+        {
+            return View("Persona",persona);
+        }
+    }
+}
